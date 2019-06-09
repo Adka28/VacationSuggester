f7a29dfb3c177ef56b55bf4de0645e722fe8f39e
Andrea Szaboova <ada2801@live.com>
2019-06-09 00:23:22 -0700

    Added initial form and java script code

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#survey form").submit(function(event) {
+      var name1Input = $("input#name1").val();
+  
+      $(".name1").text(name1Input);
+  
+      $("#suggestion").show();
+  
+      event.preventDefault();
+    });
+  });
